055d147630632ca543580963cf3d17cd43c9c6eb
Julio Neto <nettojulio@hotmail.com>
2024-09-11 19:22:27 -0300

    Revert "(fix):commit 38faa59345"
    This reverts commit 121c20771958f1df07aa1355b96e76a978b42c81.

--- src/main/java/br/com/gjnv/petshop/dto/EnderecoDto.java
+++ src/main/java/br/com/gjnv/petshop/dto/EnderecoDto.java
@@ -1,7 +1,5 @@
 package br.com.gjnv.petshop.dto;
-import lombok.Data;
 
-@Data
 public class EnderecoDto {
 
     private Long id;
@@ -11,4 +9,51 @@ public class EnderecoDto {
     private String cidade;
     private String complemento;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 }
