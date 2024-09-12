e85ef795549b2bb5c30175211c95abc2d1a37b0b
gabrielZZ231 <gabriel1235897@gmail.com>
2024-09-11 23:24:30 -0300

    wip

--- src/main/java/br/com/gjnv/petshop/dto/ClienteDto.java
+++ src/main/java/br/com/gjnv/petshop/dto/ClienteDto.java
@@ -1,13 +1,42 @@
 package br.com.gjnv.petshop.dto;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+
 public class ClienteDto {
     private Long id;
     private String nome;
     private String telefone;
     private String cpf;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
 
--- src/main/java/br/com/gjnv/petshop/dto/VagaDto.java
+++ src/main/java/br/com/gjnv/petshop/dto/VagaDto.java
@@ -1,15 +1,45 @@
 package br.com.gjnv.petshop.dto;
 
-import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-@Data
 public class VagaDto {
 
     private Integer id;
     private LocalDate data;
     private boolean horarioDisponivel;
     private LocalTime horario;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public boolean isHorarioDisponivel() {
+        return horarioDisponivel;
+    }
+
+    public void setHorarioDisponivel(boolean horarioDisponivel) {
+        this.horarioDisponivel = horarioDisponivel;
+    }
+
+    public LocalTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalTime horario) {
+        this.horario = horario;
+    }
 }
