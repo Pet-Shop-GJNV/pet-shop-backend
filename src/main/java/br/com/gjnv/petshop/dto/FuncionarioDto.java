45b9ba49445bb1f7801323962a356adbbc713001
gabrielZZ231 <gabriel1235897@gmail.com>
2024-09-11 23:24:30 -0300

    wip

--- src/main/java/br/com/gjnv/petshop/dto/FuncionarioDto.java
+++ src/main/java/br/com/gjnv/petshop/dto/FuncionarioDto.java
@@ -1,14 +1,51 @@
 package br.com.gjnv.petshop.dto;
-import lombok.Getter;
-import lombok.Setter;
+
 import java.util.UUID;
 
-@Getter
-@Setter
 public class FuncionarioDto {
     private UUID id;
     private String nome;
     private String cpf;
     private String telefone;
     private String cargo;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
 }
